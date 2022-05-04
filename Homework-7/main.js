bfb7150fa0a1762a9f5fe0864c95033b7484fa90
Ilya Fatyuschenkov <ifatyusch@gmail.com>
2022-05-04 11:33:19 +0300

    Homework 7

---
+++ Homework-7/main.js
@@ -0,0 +1,52 @@
+// Задание 2:
+
+function Cat() {
+    var foodAmount = 50;
+
+    function formatFoodAmount() {
+        return foodAmount + 'гр.'
+    }
+
+    this.feed = function() {
+       return console.log('Насыпаем в миску ' + formatFoodAmount() + ' корма.')
+    }
+}
+
+var cat = new Cat();
+
+cat.feed();
+
+// Задание 3:
+
+function Cat() {
+    var foodAmount = 50;
+
+    function getFormatFoodAmount() {
+        return foodAmount + 'гр.'
+    }
+
+    this.feed = function() {
+       return console.log('Насыпаем в миску ' + getFormatFoodAmount() + ' корма.')
+    }
+
+    this.dailyNorm = function(amount) {
+        if (!arguments.length) {
+        return foodAmount
+        }
+
+        if (amount < 50) {
+            throw new Error('Значение должно быть больше 50');
+        }
+
+        if (amount > 100) {
+            throw new Error('Значение должно быть меньше 100');
+        }
+
+       return foodAmount = amount;
+    }
+}
+
+var cat = new Cat();
+
+cat.dailyNorm(70);
+cat.feed();
